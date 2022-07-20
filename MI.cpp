d08fb656e84619f2c960cc428f2102e3dec3e958
Hartej Singh <86732914+HartejSingh2001@users.noreply.github.com>
2022-07-20 12:51:46 +0530

    Update MI.cpp

--- MI.cpp
+++ MI.cpp
@@ -12,3 +12,23 @@ void mirror(struct Node* node) {
        temp->right=t;
     }
 }
+
+// Leetcode solution
+/*
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root!=NULL)
+        {
+            invertTree(root->left);
+            invertTree(root->right);
+            TreeNode*t=root->left;
+            root->left=root->right;
+            root->right=t;
+        }
+        return root;
+    }
+};
+
+
+*/
